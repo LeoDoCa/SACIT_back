d5c9abba2bc28a95cb3eb183145a92b58e5ab51d
LeoDoCa <20223tn049@utez.edu.mx>
2025-03-16 11:59:20 -0600

    feat(Todo el sistema): Configuración inicial del repositorio de GitHub
    Creación del repositorio e inicio del backend del sistema SACIT
    
    Author: LeoDoCa

---
+++ SACIT/SACIT/src/main/java/mx/edu/utez/SACIT/SacitApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.SACIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacitApplication.class, args);
+	}
+
+}
