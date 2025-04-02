9d915ef4bdb0a80d1dc39c3ab578f01b5eb1ddb1
LeoDoCa <20223tn049@utez.edu.mx>
2025-04-02 09:17:05 -0600

    fix(mx.edu.utez.SACIT): Fix the role repository
    I delete one method with no usages in the role repository
    
    Author: LeoDoCa

--- SACIT/SACIT/src/main/java/mx/edu/utez/SACIT/repository/RoleRepository.java
+++ SACIT/SACIT/src/main/java/mx/edu/utez/SACIT/repository/RoleRepository.java
@@ -4,5 +4,4 @@ import mx.edu.utez.SACIT.model.RoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<RoleModel, Integer> {
-    RoleModel findByName(String name);
 }
