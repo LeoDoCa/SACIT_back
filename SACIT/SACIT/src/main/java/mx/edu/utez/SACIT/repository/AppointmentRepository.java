755a762ef814e26b6d1fc145a7f0a359572a0add
Moises <20223tn055@utez.edu.mx>
2025-04-09 04:27:19 -0600

    Revert "feat(services,controllers):Procedure and RequiredDocuments crud"
    This reverts commit 20c4e03b90718db1719d54da1418c504a0505635.

--- SACIT/SACIT/src/main/java/mx/edu/utez/SACIT/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.edu.utez.SACIT.repository;
-
-import mx.edu.utez.SACIT.model.Appointments;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface AppointmentRepository extends JpaRepository<Appointments, Integer> {
-    Appointments findByUuid(UUID uuid);
-}
