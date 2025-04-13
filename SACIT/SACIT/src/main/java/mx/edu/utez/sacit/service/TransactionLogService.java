0902d722f0b18cbc966bfe8856c811b8375c96b0
DiegoJesuP <spartfomi@outlook.com>
2025-04-13 13:13:04 -0600

    add transaction logbook for user actions

--- SACIT/SACIT/src/main/java/mx/edu/utez/sacit/service/TransactionLogService.java
+++ SACIT/SACIT/src/main/java/mx/edu/utez/sacit/service/TransactionLogService.java
@@ -17,16 +17,14 @@ public class TransactionLogService {
     }
 
     public void logTransaction(String transactionType, String tableName, Integer relatedUserId, String details) {
-        // Crea una nueva instancia de TransactionLog
         TransactionLog log = new TransactionLog();
-        log.setUuid(UUID.randomUUID().toString());  // Cambio aquí, de UUID a String
+        log.setUuid(UUID.randomUUID().toString());
         log.setTransactionType(transactionType);
         log.setTableName(tableName);
         log.setRelatedUserId(relatedUserId);
         log.setDetails(details);
         log.setTransactionDate(LocalDateTime.now());
 
-        // Guarda el log en la base de datos
         repository.save(log);
     }
 }
