708e90361f9e70874ca3938ec4e3aa6c6a3b470a
LeoDoCa <129547979+LeoDoCa@users.noreply.github.com>
2025-04-14 16:20:22 -0600

    Update EmailController.java

--- SACIT/SACIT/src/main/java/mx/edu/utez/sacit/controller/EmailController.java
+++ SACIT/SACIT/src/main/java/mx/edu/utez/sacit/controller/EmailController.java
@@ -13,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @MultipartConfig
 public class EmailController {
